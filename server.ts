fd2e74df48ac655c69efd174c2fbd712f09b969b
Olujuwon Alabi <ayoalabi0@gmail.com>
2023-04-24 13:44:10 +0300

    Update server.ts

--- server.ts
+++ server.ts
@@ -14,6 +14,7 @@ const port = process.env.PORT ? Number(process.env.PORT) : 8002;
 
 fastify.register(require("fastify-knexjs"), {
   client: process.env.DB_CLIENT,
+  version: "15",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
